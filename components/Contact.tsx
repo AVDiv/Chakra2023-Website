60559d5104c23026d3ca81f68fb49c427ade1081
Avin Divakara <divakaraavin@gmail.com>
2023-10-02 11:07:21 +0530

    Position update

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -3,9 +3,9 @@ import TypeIt from "typeit-react";
 import { motion } from "framer-motion";
 import { ContactCard } from "./cards/ContactCard";
 import SrimalImg from "@/public/Srimal.jpeg"
-import RashmikaImg from "@/public/Rashmika.jpg"
 import KaveenImg from "@/public/Kaveen.jpeg"
-import SeniyaImg from "@/public/Seniya.jpeg"
+import JayasankaImg from "@/public/Jayasanka.jpeg"
+import PasadiImg from "@/public/pasadi.jpg"
 
 
 export function Contact() {  
@@ -19,10 +19,10 @@ export function Contact() {
                     <div className="w-full flex justify-center py-5">
                         <ContactCard name="srimal fernando" position="- event chair -" image={SrimalImg} email="srimal.cyber@gmail.com" phone="0765341551" linkedin="https://www.linkedin.com/in/srimal-online/"/>
                     </div>
-                    <div className="grid grid-cols-1 lg:grid-cols-1 w-full justify-items-center py-5 gap-y-5">
+                    <div className="grid grid-cols-1 lg:grid-cols-3 w-full justify-items-center py-5 gap-y-5">
                         <ContactCard name="kaveen rupasinghe" position="- head of content -" image={KaveenImg} phone="0774711560" email="kaveenrupasinghe@ieee.org" linkedin="https://lk.linkedin.com/in/kaveen-rupasinghe-356123186"/>
-                        {/* <ContactCard name="chamal rashmika " position="- head of program -" image={RashmikaImg} className="object-top" phone="0710726949" email="lkrashmika@gmail.com" linkedin="https://www.linkedin.com/in/rashmikalkx/"/> */}
-                        {/* <ContactCard name="seniya wickramarachchi" position="- head of er -" image={SeniyaImg} phone="0716843121" email="seniyadewmina@ieee.org" linkedin="https://www.linkedin.com/in/seniyadewmina"/> */}
+                        <ContactCard name="jayasanka ariyarathna " position="- head of program -" image={JayasankaImg} phone="0704821254" email="jayasankaariyaratne@ieee.org" linkedin="https://www.linkedin.com/in/jayasanka-ariyaratne"/>
+                        <ContactCard name="pasandi samarasinghe" position="- head of er -" image={PasadiImg} phone="0711476371" email="pasadisamarasinghe@ieee.org" linkedin="https://www.linkedin.com/in/pasadi-samarasinghe"/>
                     </div>
                 </motion.div>
             </div>
